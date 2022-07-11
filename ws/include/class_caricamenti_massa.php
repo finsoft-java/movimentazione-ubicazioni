507ca33707883a8f28ac7dc0ca5cbf27533cf82a
Luca Vercelli <luca.vercelli@it-present.com>
2022-07-11 17:50:52 +0200

    comandi CM base

--- ws/include/class_caricamenti_massa.php
+++ ws/include/class_caricamenti_massa.php
@@ -2,8 +2,23 @@
 
 $caricamentiMassaManager = new CaricamentiMassaManager();
 
+// costanti da concordare
+// e poi da spostare nel config.php
+$CAU_TESTATA = 'XXX';
+$CAU_RIGA = 'YYY';
+$NUMERATORE = 'ZZ';
+
 class CaricamentiMassaManager {
-    
+
+    function getIdCaricamento() {
+        // FIXME ROLLBACK necessario? 
+        $panthera->executeUpdate("BEGIN TRANSACTION");
+        $id = $panthera->select_single_value("SELECT LAST_NUMBER FROM THERA.NUMERATOR WHERE NUMERATOR_ID='MOVUBI'");
+        $panthera->executeUpdate("UPDATE THERA.NUMERATOR SET LAST_NUMBER=LAST_NUMBER+1 WHERE NUMERATOR_ID='MOVUBI'");
+        $panthera->executeUpdate("END TRANSACTION");
+        return $id;
+    }
+
     function trasferisciArticolo($codUbicazioneSrc, $codUbicazioneDest, $qty) {
         global $panthera;
 
@@ -11,11 +26,278 @@ class CaricamentiMassaManager {
             return;
         }
 
-        // CM_DOC_TRA_TES/CM_DOC_TRA_RIG
-        // oppure
-        // CM_MOVIM_MAGAZ
+        $id = $this->getIdCaricamento();
+        
+        // CM_DOC_TRA_TES
+        $this->creaTestataDocumento($id, $codUbicazioneSrc, $codUbicazioneDest);
 
-        $sql = "INSERT ... BOH ...";
+        // CM_DOC_TRA_RIG
+        $this->creaRigheDocumento($id, $codUbicazioneSrc, $codUbicazioneDest, $qty);
+
+        // serve anche CM_DOC_TRA_LOT ?
+
+        // ora dovrei invocare il webservice che innesca il job CM
+      }
+
+      function creaTestataDocumento($id, $codUbicazioneSrc, $codUbicazioneDest) {
+        $sql = "INSERT INTO CM_DOC_TRA_TES (
+          DATA_ORIGIN,
+          RUN_ID,
+          ROW_ID,
+          RUN_ACTION,
+          TRASF_STATUS,
+          STATO_AVANZAMENTO,
+          ID_AZIENDA,
+          R_CAU_DOC_TRA,
+          ID_ANNO_DOC,
+          ID_ORIGINALE,
+          ID_NUMERO_DOC,
+          DATA_DOC,
+          NUMERO_DOC_FMT,
+          R_MAGAZZINO,
+          R_MAGAZZINO_ARR,
+          R_COMMESSA,
+          RIFER_DOC,
+          DTA_RIFER_DOC,
+          WF_CLASS_ID,
+          WF_ID,
+          WF_NODE_ID,
+          WF_SUB_NODE_ID,
+          NOTA,
+          R_GES_COMMENTI,
+          R_DOCUMENTO_MM,
+          R_CENTRO_COSTO,
+          R_CENTRO_RICAVO,
+          COL_MAGAZZINO,
+          LIS_CTL_STP_DOC,
+          R_GRP_CNT_CA,
+          FLAG_RIS_UTE_1,
+          FLAG_RIS_UTE_2,
+          FLAG_RIS_UTE_3,
+          FLAG_RIS_UTE_4,
+          FLAG_RIS_UTE_5,
+          STRINGA_RIS_UTE_1,
+          STRINGA_RIS_UTE_2,
+          NUM_RIS_UTE_1,
+          NUM_RIS_UTE_2,
+          STATO,
+          R_UTENTE_CRZ,
+          R_UTENTE_AGG,
+          TIMESTAMP_CRZ,
+          TIMESTAMP_AGG,
+          R_COMMESSA_ARR,
+          ANNO_DOC_RIF,
+          NUMERO_DOC_RIF,
+          TIPO_DOC_RIF,
+          DA_LOGIS_LIGHT,
+          DA_GEST_DOC_ORIG,
+          ID_ANNO_DOC_PPL,
+          ID_NUMERO_DOC_PPL,
+          ID_RIGA_DOC_PPL,
+          ID_DET_RIGA_DOC_PPL,
+          ID_RIGA_LOTTO_PPL,
+          ID_LISTA_PRL,
+          ID_RIGA_LISTA_PRL,
+          R_CLIENTE,
+          R_CLIENTE_ARR,
+          R_FORNITORE,
+          R_FORNITORE_ARR)
+        VALUES (
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        )
+        ";
         $this->execute_update($sql);
     }
+
+    function creaRigheDocumento($id, $codUbicazioneSrc, $codUbicazioneDest, $qty) {
+      $sql = "INSERT INTO CM_DOC_TRA_RIG (
+        DATA_ORIGIN,
+        RUN_ID,
+        ROW_ID,
+        RUN_ACTION,
+        TRASF_STATUS,
+        STATO_AVANZAMENTO,
+        ID_AZIENDA,
+        ID_ANNO_DOC,
+        ID_NUMERO_DOC,
+        ID_ORIGINALE,
+        ID_RIGA_DOC,
+        ID_DET_RIGA_DOC,
+        SPL_RIGA,
+        SEQUENZA_RIGA,
+        R_CAU_RIG_DOCTRA,
+        DATA_REGISTRAZIONE,
+        R_MAGAZZINO,
+        R_MAGAZZINO_ARR,
+        R_ARTICOLO,
+        R_VERSIONE,
+        R_CONFIGURAZIONE,
+        R_OPERAZIONE,
+        R_COMMESSA,
+        DES_ARTICOLO,
+        RIFER_DOC,
+        DTA_RIFER_DOC,
+        NOTA,
+        R_GES_COMMENTI,
+        R_DOCUMENTO_MM,
+        R_UM_PRM,
+        QTA_UM_PRM,
+        R_UM_SEC,
+        QTA_UM_SEC,
+        FTT_CONVER_UM,
+        OPER_CONVER_UM,
+        R_CENTRO_COSTO,
+        R_CENTRO_RICAVO,
+        R_GRP_CNT_CA,
+        FLAG_RIS_UTE_1,
+        FLAG_RIS_UTE_2,
+        FLAG_RIS_UTE_3,
+        FLAG_RIS_UTE_4,
+        FLAG_RIS_UTE_5,
+        STRINGA_RIS_UTE_1,
+        STRINGA_RIS_UTE_2,
+        NUM_RIS_UTE_1,
+        NUM_RIS_UTE_2,
+        STATO,
+        R_UTENTE_CRZ,
+        R_UTENTE_AGG,
+        TIMESTAMP_CRZ,
+        TIMESTAMP_AGG,
+        COSTO,
+        R_COMMESSA_ARR,
+        NUM_RIGA_DOC_RIF,
+        DET_RIGA_DOC_RIF,
+        R_UBI_PAR,
+        R_UBI_ARR,
+        R_CLIENTE,
+        R_CLIENTE_ARR,
+        R_FORNITORE,
+        R_FORNITORE_ARR)
+      VALUES (
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+      )
+      ";
+      $this->execute_update($sql);
+  }
 }
