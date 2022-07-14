98a5f89dc3f618922f575b9005d8bef50777f538
Federica Pistillo <pistillo.federica@libero.it>
2022-07-14 15:11:40 +0200

    aaa

--- assets/js/cambioMagazzinoUbicazione.js
+++ assets/js/cambioMagazzinoUbicazione.js
@@ -23,7 +23,6 @@ document.getElementById("qrcode").addEventListener("keyup", function(event) {
                 dataType: 'json',
                 success: function(data, status) {
                     let dati = data["data"];
-                    console.log(data);
                     let datiStampati = "";
                         datiStampati += "<p class='pOsai'> Magazzino: <strong>"+dati[0].ID_MAGAZZINO+"</strong></p>";
                         datiStampati += "<p class='pOsai'> Codice ubicazione: <strong>"+dati[0].ID_UBICAZIONE+"</strong></p>";
@@ -34,10 +33,13 @@ document.getElementById("qrcode").addEventListener("keyup", function(event) {
                             dataType: 'json',
                             success: function(data, status) { 
                                 let dati = data["data"];
-                                alert("aaaaaa");
                                 let datiStampati = "";
-                                console.log("gshhfgsdyh",data);
-                                datiStampati += "<select class=\"form-control\"><option>Mag 1</option><option>Mag 2</option><option>Mag 3</option></select>"                     
+                                datiStampati += "<select placeholder='Seleziona un magazzino' onclick='"+ (() => this.timerOn = false) +"' class=\"form-control\">";            
+                                for(let i=0; i<dati.length; i++) {
+                                    datiStampati += "<option value='"+dati[i]+"'>" + dati[i] + "</option>";                    
+                                 } 
+                                datiStampati+="</select>";
+                                $("#magazzinoDest").html("<p class='pOsai'> Magazzino destinazione: <strong>" + barCode + " </strong> </p>");
                                 $("#btnCambio").attr('disabled',false);
                                 $("#appendData").append(datiStampati);
                             }
@@ -45,20 +47,22 @@ document.getElementById("qrcode").addEventListener("keyup", function(event) {
                     }
                 });
             }
-            // if(i == 2) {
-            // $("#magazzinoDest").html("<p class='pOsai'> Magazzino destinazione: <strong>" + barCode + " </strong> </p>");
-        //     sessionStorage.setItem('ubicazione-destinazione', barCode);
-        // }
+            if(i == 2) {
+            sessionStorage.setItem('ubicazione-destinazione', barCode);
+            $("#magazzinoDest").html("<p class='pOsai'> Magazzino destinazione: <strong>" + barCode + " </strong> </p>");
+        }
     }
 });
 
 function cambioMagazzinoUbicazione() {
     timerOn = true;
+    let magazzinoDest = $("");
     $("#qrcode").attr("disabled", true);
     $.post({
-        url: "./ws/CambioMagazzinoUbicazione.php?codUbicazione=" + sessionStorage.getItem('ubicazione') + "&codUbicazioneDest=" + sessionStorage.getItem('ubicazione-destinazione'),
+        url: "./ws/CambioMagazzinoUbicazione.php?codUbicazione=" + sessionStorage.getItem('ubicazione') + "&codMagazzinoDest=" + sessionStorage.getItem('ubicazione-destinazione'),
         dataType: 'json',
         success: function(data, status) {
+            console.log("ushdusd",data);
             $("#magazzinoDest").append("<div style='display: block' class='alert alert-success' role='alert'> Cambio avvenuto con successo al magazzino <strong>"+sessionStorage.getItem('ubicazione-destinazione')+"</strong></div>");
         }
     });
