da8345d7652f83335f95b2fd6c663f9243514d90
Luca Vercelli <luca.vercelli@it-present.com>
2022-08-24 09:11:56 +0200

    bugfix

--- ws/login.php
+++ ws/login.php
@@ -1,9 +1,7 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 include("./include/all.php");
 use Firebase\JWT\JWT;
+$panthera->connect();
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     //do nothing, HTTP 200
